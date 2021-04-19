2700e8be384e9deef3a8a11e48aff99032109e3f
hc6674 <47126275+hc6674@users.noreply.github.com>
2021-04-19 17:09:16 -0400

    Update vscodesetup.sh

--- vscodesetup.sh
+++ vscodesetup.sh
@@ -5,7 +5,7 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signedby=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt install code
-wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoftprod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoftprod.deb -O packages-microsoftprod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt install -y apt-transport-https
 sudo apt install -y dotnet-sdk-3.1
