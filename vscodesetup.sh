49fa7e32837ef0ba01215154af0ae69dcb9e6ad0
hc6674 <47126275+hc6674@users.noreply.github.com>
2021-04-19 16:53:22 -0400

    Update vscodesetup.sh

--- vscodesetup.sh
+++ vscodesetup.sh
@@ -2,8 +2,7 @@
 sudo apt update
 sudo apt install software-properties-common apt-transport-https
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg
-/etc/apt/trusted.gpg.d/
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signedby=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt install code
 wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoftprod.deb -O packages-microsoft-prod.deb
