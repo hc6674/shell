b239f50cd42b3c0968cefd69c571eb38479ae682
hc6674 <47126275+hc6674@users.noreply.github.com>
2021-04-19 16:43:59 -0400

    Create vscodesetup.sh

---
+++ vscodesetup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+sudo apt update
+sudo apt install software-properties-common apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg
+/etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signedby=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install code
+wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoftprod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt install -y apt-transport-https
+sudo apt install -y dotnet-sdk-3.1
+sudo apt install -y dotnet-sdk-5.0
+sudo apt update
